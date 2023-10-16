64dc722885bfd29da9afe696b7c2e71836f0e43f
l3LUEPHOENIX <ggibson3@angelo.edu>
2023-10-16 14:55:28 -0500

    intro iptables; err-hand setupMongo.js; mongod.conf

--- mongo/run.sh
+++ mongo/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 # Start mongo, run the setup script, follow the log
-nohup mongod --bind_ip 0.0.0.0 --port 27017 --auth --fork --logpath ./mongod.log && mongo < ./setupMongo.js && tail -f ./mongod.log
+# --bind_ip 0.0.0.0 --port 27017 --auth --fork --logpath ./mongod.log
+# iptables -I INPUT -s 192.168.1.2,127.0.0.1 -p tcp --destination-port 27017 -m state --state NEW,ESTABLISHED -j ACCEPT
+# iptables -I OUTPUT -d 192.168.1.2,127.0.0.1 -p tcp --source-port 27017 -m state --state ESTABLISHED -j ACCEPT
+# iptables -A INPUT -j DROP
+# iptables -A OUTPUT -j DROP
+# iptables-save
+nohup mongod -f /opt/mongodb/mongod.conf && mongo < ./setupMongo.js && tail -f ./mongod.log
--- mongo/setupMongo.js
+++ mongo/setupMongo.js
@@ -1,13 +1,16 @@
 use vision_db;
 use admin;
-db.createUser(
-    {
-        user: 'root',
-        pwd: 'e61ddc9534f8efd85b6cd91a661fef9f',
-        roles: [
-            "userAdminAnyDatabase",
-            "dbAdminAnyDatabase",
-            "readWriteAnyDatabase"
-        ]
-    }
-)
+
+if (!db.auth('root','e61ddc9534f8efd85b6cd91a661fef9f')) {
+    db.createUser(
+        {
+            user: 'root',
+            pwd: 'e61ddc9534f8efd85b6cd91a661fef9f',
+            roles: [
+                "userAdminAnyDatabase",
+                "dbAdminAnyDatabase",
+                "readWriteAnyDatabase"
+            ]
+        }
+    )
+}
