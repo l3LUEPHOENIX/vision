b9332cbad51a9cc438c470809dbb06f4b4122b71
l3LUEPHOENIX <ggibson3@angelo.edu>
2023-10-19 23:21:56 -0500

    got group query and sessions to work; Next -> TLS

--- vision/auth.py
+++ vision/auth.py
@@ -12,14 +12,14 @@ ad_vision_group = 'CN=vision_users,OU=Groups,' + ad_base_dn
 
 def authenticate_user(username, password):
     try:
-        with Connection(ad_server, user=f'CN={username},{ad_base_dn}', password=password) as conn:
+        with Connection(ad_server, user=f'CN={username},OU=users,{ad_base_dn}', password=password) as conn:
             if conn.bind():
                 if conn.search(ad_vision_group, '(member={})'.format(conn.user), attributes=['member']):
                     return True
                 else:
-                    return False # Successful authenticate, but not member of group
+                    return False
             else:
-                return False
+                return False # Successful authenticate, but not member of group
     except Exception as e:
         # app.logger.error(f"LDAP authentication error: {str(e)}")
         return False
