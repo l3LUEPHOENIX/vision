450af281e1e63dec621ba4b6da66995806d47c55
l3LUEPHOENIX <ggibson3@angelo.edu>
2023-11-17 12:16:28 -0600

    added source search bars to archivist.html and styling

--- openldap/openldapSetup.sh
+++ openldap/openldapSetup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ldapadd -x -w abc123 -H ldaps://localhost:1389 -D "cn=vision,dc=domain3,dc=local" -f /home/openldap/vision.ldif
+ldapadd -x -w abc123 -H ldap://localhost:1389 -D "cn=vision,dc=domain3,dc=local" -f /home/openldap/vision.ldif
